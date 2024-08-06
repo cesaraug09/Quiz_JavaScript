ab8e49a10d78aecd99aaf311c63b3244cde24973
cesar <cesar.caju@academico.ifpb.edu.br>
2024-08-05 23:12:37 -0300

    varias modificações visuais e adição de funcoes

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-
-
 let min = 1; let max = 40;
+let questoes = 0;
 let vetor = [];
 let i = 0;
-var nome = '';
 const quiz = 'Perguntas_QUIZ.json';
 const container = document.querySelector('.container');
-var linkFotoUser = ''
-var lvlUser = '0';
-var diasCont = '0';
-var porcAcertos='0';
-
-
-
+var linkFotoUser;
+localStorage.getItem('lvlUser');
+localStorage.getItem('diasCont');
+localStorage.getItem('porcAcertos');
 
 main();
+
+function RegistrarUsuario(){
+    return localStorage.getItem('nomeGuardado');
+}
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -28,24 +27,12 @@ function GerarQuestoesAleatorias() {
             i++;
         }
     }
-    RegistrarQuestoes();
 }
 
 function ImprimirPergunta(pergunta){
         const h1 = document.createElement('h1');
         h1.innerText = pergunta.pergunta;
         quadrado.appendChild(h1);
-}
-
-async function RegistrarQuestoes() {
-        const response = await fetch(quiz);
-        const arquivoJSON = await response.json();
-
-        arquivoJSON.forEach(pergunta => {
-            if (vetor.includes(pergunta.id)) {
-                ImprimirPergunta(pergunta);
-            }
-        });
 }
 
 function TelaComeçar(){
@@ -66,6 +53,11 @@ const inputNome = document.createElement('input');
 const botaoOk = document.createElement('div');
 const contTexto = document.createElement('div');
 function Register(){
+
+    localStorage.setItem('lvlUser', '0');
+    localStorage.setItem('diasCont', '0');
+    localStorage.setItem('porcAcertos', '0');
+
     ApagarTela();
     const titulo = document.createElement('h1');
     contTexto.setAttribute('class', 'contTexto');
@@ -92,7 +84,7 @@ function Register(){
         if(inputNome.value==''){
             alert('Por favor, digite seu nome para continuar');
         }else{
-        nome = inputNome.value;
+        localStorage.setItem('nomeGuardado', `${inputNome.value}`);
         AdicionarFoto(inputNome);
         }
     })
@@ -103,7 +95,7 @@ function AdicionarFoto(){
     const titulo = document.createElement('h1');
     contTexto.setAttribute('class', 'contTexto');
     titulo.setAttribute('class', 'texto');
-    titulo.innerHTML = `Olá, ${nome}!<br>Por favor, escolha sua foto.`;
+    titulo.innerHTML = `Olá, ${localStorage.getItem("nomeGuardado")}!<br>Por favor, escolha sua foto.`;
     container.appendChild(titulo);
 
     container.style.justifyContent= 'space-evenly';
@@ -135,7 +127,9 @@ function AdicionarFoto(){
             const novaFoto = new FileReader();
             novaFoto.onload = function(e) {
                 linkFotoUser = e.target.result;
+                localStorage.setItem('fotoUser', linkFotoUser);
                 img.setAttribute('src', `${linkFotoUser}`);
+                localStorage.setItem("fotoUser", `${linkFotoUser}`);
             };
             novaFoto.readAsDataURL(arquivoFOTO);
         }})
@@ -153,6 +147,7 @@ function AdicionarFoto(){
 
 function BemVindo(){
     ApagarTela();
+    
     const titulo = document.createElement('h1');
     contTexto.setAttribute('class', 'contTexto');
     titulo.setAttribute('class', 'texto');
@@ -174,7 +169,7 @@ function BemVindo(){
     const nomeUsuario = document.createElement('h1');
     nomeUsuario.setAttribute('class', 'contTexto');
     nomeUsuario.setAttribute('class', 'texto');
-    nomeUsuario.innerHTML = `${nome}`;
+    nomeUsuario.innerHTML = `${localStorage.getItem("nomeGuardado")}`;
     nomeUsuario.style.color='#3e2a86';
     nomeUsuario.style.marginTop='-120px';
     divFoto.appendChild(nomeUsuario);
@@ -185,7 +180,7 @@ function BemVindo(){
 
     var nivel = document.createElement('h1');
     nivel.setAttribute('class', 'textoSTATUS');
-    nivel.innerHTML = `Lvl:<br><br>${lvlUser}`;
+    nivel.innerHTML = `Lvl:<br><br>${localStorage.getItem('lvlUser')}`;
     divEstats.appendChild(nivel);
 
     var divEstatsplus = document.createElement('div');
@@ -194,7 +189,7 @@ function BemVindo(){
 
     var nivel = document.createElement('h1');
     nivel.setAttribute('class', 'textoSTATUS');
-    nivel.innerHTML = `SEQUÊNCIA:<br><br>${diasCont}`;
+    nivel.innerHTML = `SEQUÊNCIA:<br><br>${localStorage.getItem('diasCont')}`;
     divEstats.appendChild(nivel);
 
     var divEstatsplus1 = document.createElement('div');
@@ -203,7 +198,7 @@ function BemVindo(){
 
     var nivel = document.createElement('h1');
     nivel.setAttribute('class', 'textoSTATUS');
-    nivel.innerHTML = `Acertos:<br><br>${porcAcertos}%`;
+    nivel.innerHTML = `Acertos:<br><br>${localStorage.getItem('porcAcertos')}%`;
     divEstats.appendChild(nivel);
 
 
@@ -216,18 +211,97 @@ function BemVindo(){
 
 
     img.addEventListener('click', function(){
-        AdicionarFoto(nome);
+        AdicionarFoto();
+    })
+
+
+    botaoStart.addEventListener('click', function(){
+        PLAY();
     })
 
 }
 
+let porcentagemCarregado = 0;
+function PLAY(){
+        ApagarTela();
+        console.log(vetor[questoes]);
+
+        divCarregando = document.createElement('div');
+        divCarregando.setAttribute('class', 'carregando');
+        container.appendChild(divCarregando);
+        Carregado = document.createElement('div');
+        Carregado.setAttribute('class', 'carregado');
+        porcentagemCarregado+=9.8;
+        Carregado.style.width = `${porcentagemCarregado}%`;
+        divCarregando.appendChild(Carregado);
+
+        divPerguntas = document.createElement('div');
+        divPerguntas.setAttribute('class', 'divPerguntas');
+        container.appendChild(divPerguntas);
+
+        var numeroQuestao = document.createElement('h1');
+        numeroQuestao.setAttribute('class', 'texto');
+        numeroQuestao.style.fontSize = '20px';
+        numeroQuestao.innerHTML = `Pergunta ${questoes+1}/10`;
+        divPerguntas.appendChild(numeroQuestao);
+
+        var PerguntaText = document.createElement('h1');
+        PerguntaText.setAttribute('class', 'texto');
+        PerguntaText.style.fontSize = '30px';
+        PerguntaText.style.marginBlock = '50px';
+        PerguntaText.innerHTML = `Pergunta Foda?`;
+        divPerguntas.appendChild(PerguntaText);
+////////////////////// ALTERNATIVAS
+        var btn1 = document.createElement('div');
+        btn1.innerHTML = `Talvez eu saiba`;
+        btn1.setAttribute('class', 'botaoComeçar');
+        btn1.style.margin='5px';
+        divPerguntas.appendChild(btn1);
+
+        var btn2 = document.createElement('div');
+        btn2.innerHTML = `Seu sabo`;
+        btn2.setAttribute('class', 'botaoComeçar');
+        btn2.style.margin='5px';
+        divPerguntas.appendChild(btn2);
+
+        var btn3 = document.createElement('div');
+        btn3.innerHTML = `Num sei`;
+        btn3.setAttribute('class', 'botaoComeçar');
+        btn3.style.margin='5px';
+        divPerguntas.appendChild(btn3);
+
+        var btn4 = document.createElement('div');
+        btn4.innerHTML = `Seila`;
+        btn4.setAttribute('class', 'botaoComeçar');
+        btn4.style.margin='5px';
+        divPerguntas.appendChild(btn4);
+////////////////////// ALTERNATIVAS
+        botaoProxima = document.createElement('div');
+        botaoProxima.innerHTML = `Próximo`;
+        botaoProxima.setAttribute('class', 'botaoComeçar');
+        botaoProxima.style.margin='40px';
+        container.appendChild(botaoProxima);
+
+    botaoProxima.addEventListener('click', function(){
+        if(questoes<9){
+            questoes++;
+            PLAY();
+        }else{
+            perguntaText.innerHTML = 'fim de jogo.';
+            var lvlUser = parseInt(localStorage.getItem('lvlUser'));
+            lvlUser+=1;
+            localStorage.setItem('lvlUser', `${lvlUser}`);
+        }
+
+    })
+}
+
 function renderizarFoto(){
     const img = document.createElement('img');
-    img.setAttribute('src', `${linkFotoUser}`);
-    if(linkFotoUser!=''){
-        img.setAttribute('src', `${linkFotoUser}`);
-    } else{
+    img.setAttribute('src', `${localStorage.getItem('fotoUser')}`);
+    if(!localStorage.getItem('fotoUser')){
         img.setAttribute('src', 'profile-circle.png');
+        console.log('nenhuma')
     }
     img.setAttribute('class', 'imgprofile');
 
@@ -243,11 +317,13 @@ function ApagarTela(){
 
 function main() {
     let botaoStart = TelaComeçar();
+    GerarQuestoesAleatorias();
     botaoStart.addEventListener('click', function(){
-        if(nome==''){
+        if(!RegistrarUsuario()){
         Register();}
         else{
             BemVindo();
+
         }
     })
 
