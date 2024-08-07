b1beb1375a714082f317eb722836aa782a135ae7
cesar <cesar.caju@academico.ifpb.edu.br>
2024-08-07 12:53:39 -0300

    reformatando o código inteiro separando em funções v0.1

--- script.js
+++ script.js
@@ -11,43 +11,70 @@ const container = document.querySelector('.container');
 var linkFotoUser;
 let porcentagemCarregado = 0;
 let settingsbtn = document.querySelector('.icoconfig img');
+settingsbtn.style.top="-100px"
+
 let settingsarea = document.querySelector('.areanoclick');
 let settingsclick = false;
 let vetorContadordeDias = [];
 
+let CorBotaoPrincipal = '#3A0E6F';
+let CorBotaoSecundario = 'linear-gradient(to right, rgb(27, 192, 27), rgb(38, 156, 38))';
+let CorDivPrincipal = '#7B7787';
+let CorDivSecundaria = '#6120AA';
+let CorTextoPrincipal = '#FFFFFF';
+let CorTextoSecundario = '#3E2A86';
+const naoclicavel = document.querySelector('.areanoclick');
+
+
 let agora = new Date();
 let diaHoje = `${agora.getDate()}${agora.getMonth()+1}${agora.getFullYear()}`
 
+main();
+////////////////// PARTE DO CÓDIGO DEDICADA AO LOCALSTORAGE
+
+localStorage.setItem('lvlUser', '0');
+localStorage.setItem('diasCont', '0');
+localStorage.setItem('porcAcertos', '0');
+
 if(!localStorage.getItem('PrimeiroDiaJogado')){
-    console.log("nao tem nada");
     localStorage.setItem('PrimeiroDiaJogado', diaHoje);
     localStorage.setItem('DiasJogados', diaHoje);
     vetorContadordeDias.push(diaHoje)
-    console.log(vetorContadordeDias)
 }
 
 function RegistraDiasJogados(){
     vetorContadordeDias = parseInt(localStorage.getItem('DiasJogados'));
-    console.log(vetorContadordeDias)
     if(!vetorContadordeDias.includes(diaHoje)){
         vetorContadordeDias.push(diaHoje);
     }
-    console.log('esses sao os dias jogados')
-    console.log(vetorContadordeDias)
 }
 
 function RegistraQuestoesRespondidas(num){
     localStorage.setItem('Questoes',  num);
 }
 
-
-
-obterPergunta();
-main();
-
-function RegistrarUsuario(){
+function localStorageNomeUsuario(){
     return localStorage.getItem('nomeGuardado');
 }
+
+function ChecaTemRegistro(){
+    if(!localStorageNomeUsuario()){
+        Register();}
+    else{
+        BemVindo();
+    }
+}
+
+function LimpaLocalStorage(){
+    localStorage.clear();
+    naoclicavel.removeChild(menu);
+    naoclicavel.style.width='0%';
+    naoclicavel.style.height='0%';
+    settingsclick = false;
+    location.reload();
+}
+
+////////////////// PARTE DO CÓDIGO DEDICADA AO LOCALSTORAGE
 
 function obterNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -65,113 +92,139 @@ function GerarQuestoesAleatorias() {
     questoes=0;
 }
 
-function TelaComeçar(){
-    const logo = document.createElement('img');
-        logo.setAttribute('src', 'js.png');
-            logo.setAttribute('class', 'logo');
-                container.appendChild(logo);
+////////////////// PARTE PARA GERAR VETORES COM NUMEROS ALEATORIOS
 
-                botaoStart = document.createElement('div');
-            botaoStart.innerHTML = `>> COMEÇAR <<`;
-        botaoStart.setAttribute('class', 'botaoComeçar');
-    container.appendChild(botaoStart);
-
-    return botaoStart;
+function ImprimeIMG(src, classe, ElementoPai){
+    const img = document.createElement('img');
+        img.setAttribute('src', src);
+            img.setAttribute('class', classe);
+                ElementoPai.appendChild(img);
 }
 
-const inputNome = document.createElement('input');
-const botaoOk = document.createElement('div');
-const contTexto = document.createElement('div');
-
-function Register(){
-    ApagarTela();
-    localStorage.setItem('lvlUser', '0');
-    localStorage.setItem('diasCont', '0');
-    localStorage.setItem('porcAcertos', '0');
-
-    const titulo = document.createElement('h1');
-        contTexto.setAttribute('class', 'contTexto');
-            titulo.setAttribute('class', 'texto');
-                titulo.innerHTML = `Vamos lá, <br>qual é o seu nome?`;
-                    container.appendChild(titulo);
-                        inputNome.setAttribute('type', 'text');
-                    inputNome.setAttribute('class', 'textNome');
-                container.appendChild(contTexto);
-            contTexto.appendChild(inputNome);
-        container.style.justifyContent= 'center';
-
-    botaoOk.innerHTML = `Ok!`;
-    botaoOk.setAttribute('id', 'botaoConfirm');
-    let i = 0;
-    inputNome.addEventListener('keydown', function(){
-        if(i==0){
-            botaoOk.setAttribute('class', 'botaoConfirm');
-                contTexto.appendChild(botaoOk);}
-                    i++;
-        })
-
-    botaoOk.addEventListener('click', function(){
-        if(inputNome.value!=''){
-            localStorage.setItem('nomeGuardado', `${inputNome.value}`);
-                AdicionarFoto(inputNome);
-        }
-    })
+function Imprime(tipo, texto, classe, ElementoPai, corText, corBackGround){
+    div = document.createElement(`${tipo}`);
+    div.setAttribute('class', classe);
+    div.style.background=corBackGround;
+    if(corText){
+        div.style.color=corText;
+    }
+    if(texto){
+        div.innerHTML = texto;
+    }
+    ElementoPai.appendChild(div);
+    return div;
 }
 
-function AdicionarFoto(){
-    ApagarTela();
 
+function ImprimeTexto(texto,classe,ElementoPai, corText){
     const titulo = document.createElement('h1');
-        contTexto.setAttribute('class', 'contTexto');
-            titulo.setAttribute('class', 'texto');
-                titulo.innerHTML = `Olá, ${localStorage.getItem("nomeGuardado")}!<br>Por favor, escolha sua foto.`;
-                    container.appendChild(titulo);
+    titulo.setAttribute('class', classe);
+    titulo.innerHTML = texto;
+    titulo.style.color=corText;
+    ElementoPai.appendChild(titulo);
+}
 
-                        container.style.justifyContent= 'center';
-                    divFoto = document.createElement('div');
-                divFoto.setAttribute('class', 'divFoto');
-            container.appendChild(divFoto);
-        divFoto.style.margin='20px';
-    var img = renderizarFoto();
+function renderizarFoto(){
+    const img = document.createElement('img');
+    img.setAttribute('class', 'imgprofile');
+        img.setAttribute('src', `${localStorage.getItem('fotoUser')}`);
+    if(!localStorage.getItem('fotoUser')){
+        img.setAttribute('src', 'profile-circle.png');
+    }
 
-    botaoEnviar = document.createElement('div');
-        botaoEnviar.innerHTML = `ENVIAR FOTO`;
-            botaoEnviar.setAttribute('class', 'botaoComeçar');
-                botaoEnviar.style.background = 'linear-gradient(to left, green, #29c029)';
-                    divFoto.appendChild(botaoEnviar);
+    divFoto.appendChild(img);
+        return img;
+}
 
-                    const inputFoto = document.createElement('input');
-                inputFoto.setAttribute('type', 'file');
-            inputFoto.setAttribute('class', 'inputfoto');
-        inputFoto.style.display = 'none';
+function EnviaEMudaFoto(img){
+    const inputFoto = document.createElement('input');
+    inputFoto.setAttribute('type', 'file');
+    inputFoto.setAttribute('class', 'inputfoto');
+    inputFoto.style.display = 'none';
     container.appendChild(inputFoto);
 
-    botaoEnviar.addEventListener('click', function(){
-        inputFoto.click();
-    })
+    inputFoto.click();
 
     inputFoto.addEventListener('change', function(event) {
         const arquivoFOTO = event.target.files[0];
         if (arquivoFOTO) {
             const novaFoto = new FileReader();
             novaFoto.onload = function(e) {
-                linkFotoUser = e.target.result;
-                    localStorage.setItem('fotoUser', linkFotoUser);
-                        img.setAttribute('src', `${linkFotoUser}`);
-                            localStorage.setItem("fotoUser", `${linkFotoUser}`);
+            linkFotoUser = e.target.result;
+            localStorage.setItem('fotoUser', linkFotoUser);
+            img.setAttribute('src', `${linkFotoUser}`);
+            localStorage.setItem("fotoUser", `${linkFotoUser}`);
             };
-            novaFoto.readAsDataURL(arquivoFOTO);
-        }})
-
-                botaoStart = document.createElement('div');
-            botaoStart.innerHTML = `CONTINUAR`;
-        botaoStart.setAttribute('class', 'botaoComeçar');
-    divFoto.appendChild(botaoStart);
-
-    botaoStart.addEventListener('click', function(){
-            BemVindo();
+        novaFoto.readAsDataURL(arquivoFOTO);
+        }
     })
 }
+
+function GeraInputTexto(){
+    inputNome.setAttribute('class', 'textNome');
+    contTexto.setAttribute('class', 'contTexto');
+    contTexto.appendChild(inputNome);
+    container.appendChild(contTexto);
+    botaoOk.innerHTML = `Ok!`;
+    botaoOk.setAttribute('id', 'botaoConfirm');
+}
+
+////////////////// PARTE VISUAL DO FRONT
+
+
+function TelaComeçar(){
+    ImprimeIMG('js.png', 'logo', container);
+    obterPergunta();
+
+    var btnComeçar = Imprime('div','>> COMEÇAR <<', 'botaoComeçar', container, CorTextoPrincipal, CorBotaoPrincipal);
+
+    btnComeçar.addEventListener('click', function(){
+        settingsbtn.style.top="0"; ///// Faz o botao Settings Aparecer
+        ChecaTemRegistro();
+    })
+}
+
+const inputNome = document.createElement('input');
+const contTexto = document.createElement('div');
+const botaoOk = document.createElement('div');
+
+
+function Register(){
+    container.style.justifyContent= 'center';
+    ApagarTela();
+    ImprimeTexto(`Vamos lá, <br>qual é o seu nome?`, 'texto', container, CorTextoPrincipal);
+    GeraInputTexto();
+
+    let i = 0;
+    inputNome.addEventListener('keydown', function(){
+        if(i==0){
+            botaoOk.setAttribute('class', 'botaoConfirm');
+            contTexto.appendChild(botaoOk);
+            }
+        i++;
+    })
+}
+
+function AdicionarFoto(){
+    container.style.justifyContent= 'center';
+    ApagarTela();
+
+    ImprimeTexto(`Olá, ${localStorage.getItem("nomeGuardado")}!<br>Por favor, escolha sua foto.`, 'texto', container, CorTextoPrincipal);
+
+    divFoto = Imprime('div',null, 'divFoto', container, null, CorDivPrincipal);
+    divFoto.style.margin='20px';
+
+    var img = renderizarFoto();
+    botaoEnviar = Imprime('div',`ENVIAR FOTO`, 'botaoComeçar', divFoto, CorTextoPrincipal, CorBotaoSecundario);
+    botaoEnviar.addEventListener('click', function(){
+        EnviaEMudaFoto(img);
+    })
+    const btnContinue = Imprime('div', `CONTINUAR`, 'botaoComeçar', divFoto, CorTextoPrincipal, CorBotaoPrincipal);
+    btnContinue.addEventListener('click', function(){
+        BemVindo();
+    })
+}
+
 
 function BemVindo(){
     ApagarTela();
@@ -250,9 +303,7 @@ function BemVindo(){
 }
 
 async function obterPergunta(){
-        const response = await fetch("quiz.json");
-        const quiz = await response.json();
-        if(response.ok){
+        quiz = await fetchQuizData("quiz.json");
         while(questoes<10){
                 vetorPerguntas[questoes] = quiz[vetor[questoes]].pergunta;
                 alternativasA[questoes] = quiz[vetor[questoes]].respostas[0]["1"];
@@ -260,20 +311,24 @@ async function obterPergunta(){
                 alternativasC[questoes] = quiz[vetor[questoes]].respostas[0]["3"];
                 alternativasD[questoes] = quiz[vetor[questoes]].respostas[0]["4"];
                 questoes++;
-            }
-        } else{
+                    }
         questoes=0;
-        obterPergunta();
-        }
-        questoes=0;
+}
+
+async function fetchQuizData(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return await response.json();
 }
 
 
 
 async function PLAY(){
     ApagarTela();
-
-    questoes= parseInt(localStorage.getItem('Questoes'));
+    recuperarNumeroQuestao =localStorage.getItem('Questoes');
+    questoes= parseInt(recuperarNumeroQuestao);
     console.log(questoes)
         divCarregando = document.createElement('div');
             divCarregando.setAttribute('class', 'carregando');
@@ -376,17 +431,6 @@ async function PLAY(){
         }})
 }
 
-function renderizarFoto(){
-    const img = document.createElement('img');
-    img.setAttribute('class', 'imgprofile');
-        img.setAttribute('src', `${localStorage.getItem('fotoUser')}`);
-    if(!localStorage.getItem('fotoUser')){
-        img.setAttribute('src', 'profile-circle.png');
-    }
-
-    divFoto.appendChild(img);
-        return img;
-}
 
 function ApagarTela(){
     while (container.firstChild) {
@@ -394,86 +438,56 @@ function ApagarTela(){
     }
 }
 
-
-async function main(){
-    settingsbtn.style.top="-100px";
-    let botaoStart = TelaComeçar();
-        GerarQuestoesAleatorias();
-            botaoStart.addEventListener('click', function(){
-                settingsbtn.style.top="0";
-                if(!RegistrarUsuario()){
-                    Register();}
-                else{
-                    BemVindo();
-                }
-            })
-}
-
-var naoclicavel = document.querySelector('.areanoclick');
-
-settingsbtn.addEventListener('click', function(){
-    ChamaMenu();
-})
-
 function ChamaMenu(){
     if(settingsclick==false){
         naoclicavel.style.width='100vw';
         naoclicavel.style.height='100%';
 
-        menu = document.createElement('div');
-        menu.setAttribute('class', 'divFoto');
-        menu.style.background='#262626';
+        menu = Imprime('div', null, 'divFoto', naoclicavel, CorTextoPrincipal, '#262626');
         menu.style.boxShadow='0px 0px 90px black';
         menu.style.position = "absolute";
-        naoclicavel.appendChild(menu);
 
-        const titulo = document.createElement('h1');
-        contTexto.setAttribute('class', 'contTexto');
-            titulo.setAttribute('class', 'texto');
-                titulo.innerHTML = `Configurações`;
-                    menu.appendChild(titulo);
+        titulo = ImprimeTexto(`Configurações`, 'texto', menu, CorTextoPrincipal);
 
-        botaoMudarTema = document.createElement('div');
-        botaoMudarTema.innerHTML = `Mudar Tema`;
-        botaoMudarTema.style.background='#474747';
-                botaoMudarTema.setAttribute('class', 'botaoComeçar');
-                    menu.appendChild(botaoMudarTema);
+        btnMudarTema = Imprime('div',`Mudar Tema`, 'botaoComeçar', menu, CorTextoPrincipal, '#474747');
+        btnTrocaNick = Imprime('div',`Trocar nickname`, 'botaoComeçar', menu, CorTextoPrincipal, '#474747');
+        btnSair = Imprime('div',`Deslogar da conta`, 'botaoComeçar', menu, CorTextoPrincipal, '#474747');
 
-                    botaoMudarNome = document.createElement('div');
-                    botaoMudarNome.innerHTML = `Trocar nickname`;
-                    botaoMudarNome.style.background='#474747';
-                    botaoMudarNome.setAttribute('class', 'botaoComeçar');
-        menu.appendChild(botaoMudarNome);
+    btnSair.addEventListener('click', function(){
+        ApagarTela();
+         LimpaLocalStorage();
+    })
 
-        botaoSair = document.createElement('div');
-            botaoSair.innerHTML = `Deslogar da conta`;
-            botaoSair.style.background='#474747';
-                botaoSair.setAttribute('class', 'botaoComeçar');
-                    menu.appendChild(botaoSair);
-
-                    botaoSair.addEventListener('click', function(){
-                        ApagarTela();
-                        localStorage.clear();
-                        naoclicavel.removeChild(menu);
-                        naoclicavel.style.width='0%';
-                        naoclicavel.style.height='0%';
-                        settingsclick = false;
-                        location.reload();
-                    })
-                    botaoMudarNome.addEventListener('click', function(){
-                        Register();
-                        naoclicavel.removeChild(menu);
-                        naoclicavel.style.width='0%';
-                        naoclicavel.style.height='0%';
-                        settingsclick = false;
-                    })
-
-        settingsclick = true;
+    btnTrocaNick.addEventListener('click', function(){
+        Register();
+        FechaMenu();
+        })
+    settingsclick = true;
     } else{
-        menu.style.background='#7B7787';
-        naoclicavel.removeChild(menu)
-        naoclicavel.style.width='0%';
-        naoclicavel.style.height='0%';
-        settingsclick = false;
+    FechaMenu();
     }
 }
+
+function FechaMenu(){
+    naoclicavel.removeChild(menu)
+    naoclicavel.style.width='0%';
+    naoclicavel.style.height='0%';
+    settingsclick = false;
+}
+
+async function main(){
+    TelaComeçar();
+    GerarQuestoesAleatorias();
+}
+
+
+settingsbtn.addEventListener('click', function(){
+    ChamaMenu();
+})
+
+botaoOk.addEventListener('click', function(){
+    if(inputNome.value!=''){
+        localStorage.setItem('nomeGuardado', `${inputNome.value}`);
+        AdicionarFoto(inputNome);
+    }
+})
